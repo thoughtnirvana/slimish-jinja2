372ade5334eaebb546394d3d96a74bb1ec53c398
Rahul Kumar <rahulsinner@gmail.com>
2013-02-27 08:33:48 +0530

    Version bump.

--- setup.py
+++ setup.py
@@ -210,7 +210,7 @@ converts to::
 
 from setuptools import setup
 setup(name='slimish_jinja',
-      version='1.0',
+      version='1.0.1',
       packages=['slimish_jinja'],
       scripts=['slim_to_jinja.py'],
       install_requires=['distribute', 'jinja2'],
