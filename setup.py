63efd9cddf0d3ac9716fca1266b3660bf894022a
Rahul Kumar <rahul@thoughtnirvana.com>
2019-01-24 09:04:46 +0530

    Version bump for bug fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='slimish_jinja',
-    version='1.1.0',
+    version='1.1.1',
     packages=['slimish_jinja'],
     scripts=['slim_to_jinja.py'],
     install_requires=['future', 'jinja2'],
